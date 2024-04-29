7cd7de17b437795ed2657fb4016a02dedac399ec
hafskjfha <153927840+hafskjfha@users.noreply.github.com>
2024-04-29 15:52:03 +0900

    Create code.py

---
+++ 백준/1000~10000/1764 듣보잡/code.py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+N, M = map(int, input().split())
+de=set()
+bo=set()
+for _ in range(N):
+	s = input().strip()
+  de.add(s)
+for _ in range(M):
+	s = input().strip()
+	bo.add(s)
+h = de & bo
+h=sorted(h)
+print(len(h))
+for c in h:
+	print(c)
