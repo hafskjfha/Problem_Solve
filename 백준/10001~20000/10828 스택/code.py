690fd9418c4c7055518357c9a4b44e6af1ac232b
hafskjfha <153927840+hafskjfha@users.noreply.github.com>
2024-04-29 16:30:54 +0900

    Create code.py

---
+++ 백준/10001~20000/10828 스택/code.py
@@ -0,0 +1,26 @@
+from collections import deque
+import sys
+input=sys.stdin.readline
+N = int(input())
+de = deque([])
+for _ in range(N):
+	I = input().split()	
+	if I[0] == 'push':
+		de.append(int(I[1]))
+	elif I[0] == 'pop':
+		if len(de) == 0:
+			print(-1)
+		else:
+			print(de.pop())
+	elif I[0] == 'size':
+		print(len(de))
+	elif I[0] == 'empty':
+		if len(de)==0:
+			print(1)
+		else:
+			print(0)
+	elif I[0]=='top':
+		if len(de)==0:
+			print(-1)
+		else:
+			print(de[-1])
