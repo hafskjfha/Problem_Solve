6d8a5c8a581e41afe78d0dc333036c426471b985
hafskjfha <153927840+hafskjfha@users.noreply.github.com>
2024-05-05 14:22:31 +0900

    Create code2.py

---
+++ 백준/1000~10000/7107 Journey of A Knight/code2.py
@@ -0,0 +1,12 @@
+N,M,I,J=map(int,input().split());f=[[0]*N for _ in range(M)]
+def bfs():
+	q=[(0,-1,0)]
+	while q:
+		x,y,d=q.pop(0)
+		if f[y][x]==2:continue
+		if (x,y)==(I-1,-J):return d
+		f[y][x]=2
+		for dx,dy in [(x-1,y-2),(x-2,y-1),(x-2,y+1),(x-1,y+2),(x+1,y+2),(x+2,y+1),(x+2,y-1),(x+1,y-2)]:
+			if 0<=dx<N and -M<=dy<0 and f[dy][dx]==0:q.append((dx,dy,d+1))
+	return "NEVAR"
+print(bfs())
