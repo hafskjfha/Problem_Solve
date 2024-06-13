fac8ff9d57fdd1b54ba58a7e219aefcb981030d8
hafskjfha <153927840+hafskjfha@users.noreply.github.com>
2024-06-13 09:20:34 +0900

    Create code.py

---
+++ 백준/10001~20000/11444 피보나치 수6/code.py
@@ -0,0 +1,17 @@
+N=int(input())
+def mul(a,b):
+	mat=[[0,0],[0,0]]
+	for i in range(2):
+		for j in range(2):
+			for k in range(2):
+				mat[i][j]+=a[i][k] * b[k][j]
+			mat[i][j]%=int(1e9+7)
+	return mat
+rst=[[1,0],[0,1]]
+tmp=[[1,1],[1,0]]
+while N:
+	if N%2==1:
+		rst=mul(rst,tmp)
+	tmp=mul(tmp,tmp)
+	N>>=1
+print(rst[0][1])
