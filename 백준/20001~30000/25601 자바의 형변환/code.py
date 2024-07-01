9ea4bfec6a2e0662cd8e923d08cd8b133a371391
hafskjfha <153927840+hafskjfha@users.noreply.github.com>
2024-07-02 06:57:57 +0900

    code.py 업데이트

--- 백준/20001~30000/25601 자바의 형변환/code.py
+++ 백준/20001~30000/25601 자바의 형변환/code.py
@@ -2,16 +2,14 @@ import sys
 input=sys.stdin.readline
 c={}
 for _ in range(int(input())-1):
-	A,B=input().strip().split()
-	c[A]=B
+        A,B=input().strip().split()
+        c[A]=B
 def ff(C,D):
-	t=C
-	while 1:
-		if (t:=c.get(t,0))==0:break
-		if t==D:return 1
-	t=D
-	while 1:
-		if (t:=c.get(t,0))==0:break
-		if t==C:return 1
-	return 0
+        t=C
+        while t:
+                if (t:=c.get(t,0))==D:return 1
+        t=D
+        while t:
+                if (t:=c.get(t,0))==C:return 1
+        return 0
 print(ff(*input().strip().split()))
