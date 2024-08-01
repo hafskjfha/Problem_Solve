b8aa2e77fdeb6382ebe0c9eadef8a76038316abf
hafskjfha <153927840+hafskjfha@users.noreply.github.com>
2024-08-02 08:46:42 +0900

    Create code.py

---
+++ 백준/30001~40000/30801 가희와 클럽 오디션/code.py
@@ -0,0 +1,45 @@
+note=input()
+in_key=list(input())
+direction={'W':1,'A':2,'S':3,'D':4,'LU':5,'LD':6,'RU':7,'RD':8,'W!':3,'A!':4,'S!':1,'D!':2,'LU!':8,'LD!':7,'RU!':6,'RD!':5}
+key={'W':1,'8':1,'S':3,'2':3,'A':2,'4':2,'D':4,'6':4,'7':5,'1':6,'9':7,'3':8}
+N=[]
+i=0
+while i<len(note):
+    if note[i]in('L','R'):
+        temp=note[i]
+        i+=1
+        temp+=note[i]
+        i+=1
+        try:
+            if note[i]=='!':
+                i+=1
+                temp+='!'
+        except:
+            pass
+    else:
+        try:
+            if note[i+1]=='!':
+                temp=note[i]+note[i+1]
+                i+=2
+            else:
+                temp=note[i]
+                i+=1
+        except:
+            temp=note[i]
+            i+=1
+    N.append(direction[temp])
+LV=len(N)
+in_key=[key[i]for i in in_key]
+correct=[]
+i=0
+for k in in_key:
+    if i==LV:
+        i=0
+        correct=[]
+    elif N[i]==k:
+        correct.append(k)
+        i+=1
+    else:
+        correct=[]
+        i=0
+print('Yes' if len(correct)==LV else 'No')
