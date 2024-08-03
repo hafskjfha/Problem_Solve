3f99cfb04b9772d3c90481d2037b71d28fee4c44
hafskjfha <153927840+hafskjfha@users.noreply.github.com>
2024-08-03 21:35:01 +0900

    Create code.py

---
+++ 백준/1000~10000/1715 카드 정렬하기/code.py
@@ -0,0 +1,13 @@
+from heapq import heappush,heappop
+pq=[]
+input=open(0).readline
+push=heappush
+pop=heappop
+S=0
+for _ in range(int(input())):
+    push(pq,int(input()))
+while len(pq)>1:
+    k=pop(pq)+pop(pq)
+    S+=k
+    push(pq,k)
+print(S)
