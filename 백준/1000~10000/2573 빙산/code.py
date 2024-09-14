cbf3fd49421a3c10bc501b3931bbc0dca2153b87
hafskjfha <153927840+hafskjfha@users.noreply.github.com>
2024-09-14 22:23:24 +0900

    2573

--- 백준/1000~10000/2573 빙산/code.py
+++ 백준/1000~10000/2573 빙산/code.py
@@ -1,5 +1,4 @@
 from collections import deque
-from copy import deepcopy
 input=open(0).readline
 def bfs(V,A,N,M,k,sx,sy):
     c=1
@@ -7,7 +6,7 @@ def bfs(V,A,N,M,k,sx,sy):
     V[sy][sx]=k
     while q:
         x,y=q.popleft()
-        for dx,dy in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
+        for dx,dy in ((x+1,y),(x-1,y),(x,y+1),(x,y-1)):
             if 0<=dx<M and 0<=dy<N and V[dy][dx]<k and A[dy][dx]:
                 q.append((dx,dy))
                 V[dy][dx]=k
@@ -20,47 +19,47 @@ def main():
     sea=[]
     A=[]
     ice=0
+    ices=set()
+    ttemp=[[],[]] #녹일수 있는 바다 담는 리스트
     for i in range(N):
         temp=[*map(int,input().split())]
         for j in range(M):
             if temp[j]:
                 ice+=1
+                ices.add((j,i))
             else:
-                sea.append((j,i))
+                ttemp[1].append((j,i))
         A.append(temp)
     k=1
     while 1:
-        ttemp=[]
-        D=False
-        for x,y in sea:
+        u=k%2
+        D=False #얼음이 녹았는지 체크 하는 변수
+        for x,y in ttemp[u]:
             P=False
-            for dx,dy in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
+            for dx,dy in ((x+1,y),(x-1,y),(x,y+1),(x,y-1)):
                 if 0<=dx<M and 0<=dy<N and A[dy][dx]:
-                    P=True
+                    P=True #네 방향중 하나라도 얼음이 접해있는 바다
                     if A[dy][dx]:
                         A[dy][dx]-=1
                         if A[dy][dx]==0:
-                            ttemp.append((dx,dy))
-                            D=True
+                            ttemp[0 if u else 1].append((dx,dy))
+                            ices.remove((dx,dy))
+                            D=True #녹일수 있으면 true
                             ice-=1
-                            if ice==0:
+                            if ice==0: #다 녹으면 처리
                                 print(0)
                                 return 1
             if P:
-                ttemp.append((x,y))
-        sea=deepcopy(ttemp)
-        if D:
-            for i in range(N):
-                for j in range(M):
-                    if A[i][j]:
-                        V,icek=bfs(V,A,N,M,k,j,i)
-                        if icek!=ice:
-                            print(k)
-                            return 1
-                        else:break
-                else:
-                    continue
+                ttemp[0 if u else 1].append((x,y))
+        ttemp[u]=[]#바다 리스트 갱신
+        if D: #얼음이 1개 이상녹았나?
+            for i,j in ices:
+                V,icek=bfs(V,A,N,M,k,i,j)
+                if icek!=ice: #얼음덩어리 나누어짐
+                    print(k)
+                    return 1
                 break
-                
-        k+=1            
+        k+=1
+            
 main()
+
