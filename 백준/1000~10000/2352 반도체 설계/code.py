7f8a34a8bf209923ab5ae4dfb1a9bd242ee47e4a
Jung-Taewon <153927840+hafskjfha@users.noreply.github.com>
2024-10-03 16:39:30 +0900

    2352

---
+++ 백준/1000~10000/2352 반도체 설계/code.py
@@ -0,0 +1,12 @@
+from bisect import bisect_left
+
+input()
+B=[]
+L=0
+for i in map(int,input().split()):
+    if not B or B[-1]<i:
+        B.append(i)
+        L+=1
+    else:
+        B[bisect_left(B,i)]=i
+print(L)
